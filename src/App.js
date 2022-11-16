1ed6b02973c4277c54fecd4b56051f4bfb36e3c3
Edvard Eros <edvard.eros@yahoo.com>
2022-11-17 00:14:13 +0100

    OpenAI api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Configuration, OpenAIApi } from "openai";
+
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_VITE_Open_AI_Key
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "512x512"
+    });
+    setResult(res.data.data[0].url);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-main">
+      <>
+        <h2>Generate an Image using Open AI API</h2>
+
+        <textarea
+          className="app-input"
+          placeholder="Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh.."
+          onChange={(e) => setPrompt(e.target.value)}
+          rows="10"
+          cols="40"
+        />
+        <button onClick={generateImage}>Generate an Image</button>
+      </>
+      {result.length > 0 ? (
+        <img className="result-image" src={result} alt="result" />
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
